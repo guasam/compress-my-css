ef1e44e3b369beda5e3eabd61cc3e987dcb33aef
guasam <guasam@codesbiome.com>
2020-12-10 19:33:24 +0530

    create CheckBox component

---
+++ src/components/GCheckBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="GCheckBox" :class="checked ? 'GCheckBox__active' : ''" @click="handleClick">
+    <span v-html="checkBoxIcon" />
+    <input type="checkbox" v-model="checked" />
+  </div>
+</template>
+
+<script>
+import { checkBoxIcon } from "../utils/icons";
+
+export default {
+  name: "GCheckBox",
+  data() {
+    return {
+      checked: false,
+      checkBoxIcon,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.checked = !this.checked;
+    },
+  },
+};
+</script>
